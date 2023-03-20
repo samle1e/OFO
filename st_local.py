fea37b63a8d11b09a471369ef4fbb223f64c22a0
samle1e <sam.le@sba.gov>
2023-03-20 09:38:49 -0400

    Use State to Determine Region
    Rewrote script to use state name to determine region, rather than ZIP code

--- st_local.py
+++ st_local.py
@@ -15,42 +15,152 @@ st.set_page_config(
 
 #os.chdir("C:/Users/SQLe/U.S. Small Business Administration/Office of Policy Planning and Liaison (OPPL) - Data Lake/")
 #%%
-#define my datasets
-SBA_match=pd.read_csv("SBA_DO_ZIP_matching_table.csv",  converters={'ZIP.Code': str}).drop_duplicates("ZIP.Code")
-# SBA_match_pl=pl.from_pandas(SBA_match).lazy()
+#bring in the match table
+SBA_match=pd.read_csv("/SBA_DO_ZIP_matching_table.csv",  converters={ #./Mapping Files
+	'ZIP.Code': str},usecols=[
+	"ZIP.Code","SBA.District.Office","State.Name"]).drop_duplicates("ZIP.Code")
+SBA_match['VENDOR_ADDRESS_STATE_NAME']=SBA_match["State.Name"].str.upper()
+ZIP_list=SBA_match['ZIP.Code']
+
+#%%
 # SBGRdir="./SBGR_parquet"
 # list=sorted([file for file in os.listdir(SBGRdir) if "SBGR" in file])
 # max_year=list[-1].replace("SBGR_FY","")
-max_year=2022 
 # arrowds=ds.dataset(SBGRdir,format="parquet")
 # plds=pl.scan_ds(arrowds)
 
 #%%
+max_year=2022 
+
+#%%
+# dicts for regions and DOs
+Region_dict={'Connecticut':1,'Maine':1
+,'Massachusetts':1,'New Hampshire':1
+,'Rhode Island':1,'Vermont':1
+,'New Jersey':2,'New York':2
+,'Puerto Rico':2, 'Guam':2
+,'Delaware':3,'District of Columbia':3
+,'Maryland':3,'Pennsylvania':3
+,'Virginia':3,'West Virginia':3
+,'Alabama':4,'Florida':4
+,'Georgia':4,'Kentucky':4
+,'Mississippi':4,'North Carolina':4
+,'South Carolina':4
+,'Tennessee':4,'Illinois':5
+,'Indiana':5,'Michigan':5
+,'Minnesota':5
+,'Ohio':5,'Wisconsin':5
+,'Arkansas':6,'Louisiana':6
+,'New Mexico':6
+,'Oklahoma':6,'Texas':6
+,'Iowa':7,'Kansas':7
+,'Missouri':7,'Nebraska':7
+,'Colorado':8,'Montana':8
+,'North Dakota':8,'South Dakota':8
+,'Utah':8,'Wyoming':8
+,'Arizona':9,'California':9
+,'Hawaii':9,'Nevada':9
+,'Northern Marianas':9
+,'Alaska':10
+,'Idaho':10,'Oregon':10
+,'Washington':10
+}
+Region_dict_upper = {key.upper():Region_dict[key] for key in sorted(Region_dict.keys())}
+
+single_DO=['Alabama'
+,'Alaska'
+,'Arizona'
+,'Arkansas'
+,'Colorado'
+,'Connecticut'
+,'Delaware'
+#,'District of Columbia'
+,'Georgia'
+,'Hawaii'
+,'Illinois'
+,'Indiana'
+,'Iowa'
+,'Kentucky'
+,'Louisiana'
+,'Maine'
+,'Massachusetts'
+,'Michigan'
+,'Minnesota'
+,'Mississippi'
+,'Montana'
+,'Nebraska'
+,'Nevada'
+,'New Hampshire'
+,'New Jersey'
+,'New Mexico'
+,'North Carolina'
+,'North Dakota'
+,'Oklahoma'
+#,'Puerto Rico'
+,'Rhode Island'
+,'South Carolina'
+,'South Dakota'
+,'Tennessee'
+,'Utah'
+,'Vermont'
+,'West Virginia'
+,'Wisconsin'
+,'Wyoming'
+#,'American Samoa'
+#,'Guam'
+#,'Northern Marianas'
+]
+single_DO=[x.upper() for x in single_DO]
 dolcols=["TOTAL_SB_ACT_ELIGIBLE_DOLLARS","SMALL_BUSINESS_DOLLARS","SDB_DOLLARS","WOSB_DOLLARS","CER_HUBZONE_SB_DOLLARS","SRDVOB_DOLLARS"]
+
+#%%
 
 @st.cache_data
 def get_dollars():
     dollars=pd.read_parquet("dollars.parquet")
+    # plsingle_Do=pl.Series(single_DO)
+    # plziplist=pl.Series(ZIP_list)
+
     # plds_zip=plds.filter(
-    #     pl.col("FISCAL_YEAR")<=int(max_year)).with_columns(
-	#     pl.col("VENDOR_ADDRESS_ZIP_CODE").str.slice(0,5).alias("ZIP5"))
-    # dollars=plds_zip.groupby("ZIP5","FISCAL_YEAR","VENDOR_ADDRESS_STATE_NAME").agg(pl.col(dolcols).sum())
-    # dollars=dollars.join(SBA_match_pl,how="left",left_on="ZIP5",right_on='ZIP.Code').sort("FISCAL_YEAR").collect().to_pandas()
+    #     (pl.col("FISCAL_YEAR")<=int(max_year))).with_columns(
+    #     pl.col("VENDOR_ADDRESS_ZIP_CODE").str.slice(0,5).alias("ZIP5")
+    # ).with_columns(
+    #     pl.when(pl.col('VENDOR_ADDRESS_STATE_NAME').is_in(plsingle_Do))
+    #     .then(pl.col('VENDOR_ADDRESS_STATE_NAME'))
+    #     .when(pl.col('ZIP5').is_in(plziplist))
+    #     .then(pl.col('ZIP5'))
+    #     .otherwise("NA")
+    #     .alias("group"))
+    # dollars=plds_zip.filter((pl.col('group') != "NA") & (pl.col('VENDOR_ADDRESS_STATE_NAME') !="")).groupby(
+	#     "group","FISCAL_YEAR","VENDOR_ADDRESS_STATE_NAME").agg(
+	#     pl.col(dolcols).sum()).collect().to_pandas()
     return dollars
-#select(["VENDOR_ADDRESS_STATE_NAME","VENDOR_ADDRESS_ZIP_CODE","FISCAL_YEAR"],dolcols).
+
+#%%
+dollars=get_dollars()
+#dollars.to_parquet("dollars.parquet") #COMMENT OUT
+
+#%%
+dollarsDO=dollars.merge(SBA_match,how="left",left_on=["group","VENDOR_ADDRESS_STATE_NAME"],right_on=['ZIP.Code',"VENDOR_ADDRESS_STATE_NAME"])
+#%%
+#group by district office
+dollarsDO.loc[dollarsDO["VENDOR_ADDRESS_STATE_NAME"].isin(single_DO),['SBA.District.Office']]=dollarsDO["VENDOR_ADDRESS_STATE_NAME"]
+dollarsDO=dollarsDO.groupby(['SBA.District.Office',"FISCAL_YEAR","VENDOR_ADDRESS_STATE_NAME"],as_index=False)[dolcols].sum()
+dollarsDO.loc[:,'SBA.District.Office']=dollarsDO['SBA.District.Office'].str.upper()
+dollarsDO.loc[:,"SBA.Region"]=dollarsDO["VENDOR_ADDRESS_STATE_NAME"].map(Region_dict_upper)
+dollarsDO.rename(columns={"VENDOR_ADDRESS_STATE_NAME":"State"},inplace=True)
+#%%
 
 #get user input on the state/DO/Region
-state_select=pd.concat([pd.Series(["No Selection"]),SBA_match["State.Name"].dropna().drop_duplicates().sort_values()])
-region_select=pd.concat([pd.Series(["No Selection"]),SBA_match["SBA.Region"].round(0).dropna().astype(int).drop_duplicates().sort_values()])
-DO_select=pd.concat([pd.Series(["No Selection"]),SBA_match["SBA.District.Office"].dropna().drop_duplicates().sort_values()])
+def select_options(var):
+	select= pd.concat([pd.Series(["No Selection"]),dollarsDO[var].dropna().drop_duplicates().sort_values()])
+	return select
 
 st.title("Local Small Business Achievements by FY")
-state="Alabama";region="No Selection";DO="No Selection"
 
-state=st.sidebar.selectbox(label="State",options=state_select,index=1)
-region=st.sidebar.selectbox(label="Region",options=region_select,index=0)
-DO=st.sidebar.selectbox(label="SBA District",options=DO_select,index=0)
-#st.sidebar.write("Max year = ",max_year)
+state=st.sidebar.selectbox(label="State",options=select_options("State"),index=1)
+region=st.sidebar.selectbox(label="Region",options=select_options("SBA.Region"),index=0)
+DO=st.sidebar.selectbox(label="SBA District",options=select_options("SBA.District.Office"),index=0)
 
 #all dollars is initial display
 if DO != "No Selection":
@@ -58,27 +168,19 @@ if DO != "No Selection":
 elif region != "No Selection":
 	var="SBA.Region";select=region
 else:
-	var="State";select=state.upper()
+	var="State";select=state
 
-#prepare the state table
-dollars=get_dollars()
 #for dashboard, save to parquet
 #dollars.to_parquet("dollars.parquet") #COMMENT OUT
 
-
-dolcols=["TOTAL_SB_ACT_ELIGIBLE_DOLLARS","SMALL_BUSINESS_DOLLARS","SDB_DOLLARS","WOSB_DOLLARS","SRDVOB_DOLLARS","CER_HUBZONE_SB_DOLLARS"]
 #%%
-dollars=dollars.drop(columns="State").rename(columns = {'VENDOR_ADDRESS_STATE_NAME':'State'})
-#%%
-select_dollars=dollars[dollars[var]==select].groupby('FISCAL_YEAR')[dolcols].sum()
+select_dollars=dollarsDO[dollarsDO[var]==select].groupby('FISCAL_YEAR')[dolcols].sum()
 doldict={"TOTAL_SB_ACT_ELIGIBLE_DOLLARS":"Total$","SMALL_BUSINESS_DOLLARS":"SmallBusiness$","SDB_DOLLARS":"SDB$","WOSB_DOLLARS":"WOSB$","CER_HUBZONE_SB_DOLLARS":"HUBZone$","SRDVOB_DOLLARS":"SDVOSB$"}
 select_dollars=select_dollars.rename(columns=doldict)
-#%%
 #%%
 select_pct=select_dollars.iloc[:,1:].div(select_dollars.iloc[:,0], axis=0).multiply(100).round(2)
 select_pct.columns=select_pct.columns.str.replace("$","%",regex=False)
 
-#.set_axis(pct_names, axis='columns', copy=False).reset_index()
 #%%
 #show the graph
 SP_long=select_pct.melt(ignore_index=False).rename(columns={"variable":"Category","value":"Pct"})
@@ -99,7 +201,7 @@ st.write(var.replace("."," "), ":", select_display)
 select_dollars.index=select_dollars.index.astype(str)
 select_pct.index=select_pct.index.astype(str)
 
-st.table(select_dollars.style.format('$ {:,.0f}')
+st.table(select_dollars.style.format('${:,.0f}')
 	     )
 st.table(select_pct.style.format('{:.2f}%')
 )
