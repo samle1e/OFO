632a2486171076a63e0a5b8d559eb472fe1d6767
samle1e <sam.le@sba.gov>
2023-05-08 16:51:00 -0400

    Update 4_Registrants.py
    Use Snowflake data instead of uploaded data

--- pages/4_Registrants.py
+++ pages/4_Registrants.py
@@ -1,62 +1,30 @@
 #%%
-import polars as pl
 import pandas as pd
 import streamlit as st
 from datetime import datetime
-
-date_of = "20230402" #have to change every month!
+import snowflake.snowpark as sp
+from snowflake.snowpark.functions import max, col, countDistinct
 
 #%%
 def get_data ():
-    import pyarrow.dataset as ds
-    import os
-    import pyarrow as pa
-
-    datalake="C:\\Users\\SQLe\\U.S. Small Business Administration\\Office of Policy Planning and Liaison (OPPL) - Data Lake\\"
-    Extractsdir=f"{datalake}/SAM_entity_registration"
-    files = os.listdir(Extractsdir)
-    files.sort()
-    most_recent=files[-1]
-    date_of=most_recent[-8:]
-
-    arrowds=ds.dataset(f"{datalake}/SAM_entity_registration/{most_recent}",format="parquet")
-    data=pl.scan_ds(arrowds)
+    connection_parameters = st.secrets.snowflake_credentials
+    global session
+    session = sp.Session.builder.configs(connection_parameters).create()
+    data = session.table ("SAM_PUBLIC_MONTHLY_FILTERED")
+    date_of = data.select(max("LAST_UPDATE_DATE")).toPandas().iat[0,0]
     return data, date_of
 
-def create_data_local():
-    data, date_of = get_data ()
-    cols_used=["PURPOSE.OF.REGISTRATION", "BUS.TYPE.STRING", "NAICS.CODE.STRING", "NAICS.EXCEPTION.STRING"
-               , "PHYSICAL.ADDRESS.PROVINCE.OR.STATE", "PHYSICAL.ADDRESS.CITY"
-               ,"GOVT.BUS.POC.FIRST.NAME", "GOVT.BUS.POC.LAST.NAME",
-                'NAICS.EXCEPTION.COUNTER'
-                ,"LEGAL.BUSINESS.NAME",
-        "UNIQUE.ENTITY.ID",
-        'SAM.EXTRACT.CODE',
-        "PHYSICAL.ADDRESS.LINE.1",
-        "PHYSICAL.ADDRESS.LINE.2",
-        "PHYSICAL.ADDRESS.ZIP/POSTAL.CODE",
-        "ENTITY.URL","GOVT.BUS.POC.U.S..PHONE",
-        "GOVT.BUS.POC.EMAIL.",
-        "PSC.CODE.STRING",
-        'PRIMARY.NAICS']
-    data=data.select(pl.col(cols_used)).collect()
-    data.write_parquet(f"registrants_{date_of}.parquet",row_group_size=1000000,use_pyarrow=True,compression="zstd",compression_level=22)
-
-#create_data_local()
-def get_data_local ():
-    data = pl.scan_parquet(f"registrants_{date_of}.parquet")
-    return data, date_of
-
+data, date_of = get_data ()
+#%%
 def page_config (date_of):
-    date = datetime.strptime(date_of,'%Y%m%d')
     st.set_page_config(
-    page_title=f"Registrants as of {date.strftime('%d %b %Y')}",
+    page_title=f"Registrants as of {date_of.strftime('%d %b %Y')}",
     page_icon="https://www.sba.gov/brand/assets/sba/img/pages/logo/logo.svg",
    # layout="wide",
     initial_sidebar_state="expanded",
 )
-    st.header(f"SAM.gov Registrants as of {date.strftime('%d %B %Y')}")
-
+    st.header(f"SAM.gov Registrants as of {date_of.strftime('%d %B %Y')}")
+#%%
 def SAM_small_bus (data):
     options = ["All registrants","Small Business for any NAICS"
                ]
@@ -64,11 +32,11 @@ def SAM_small_bus (data):
 
     if small_bus == options[1]:
         data = data.filter(
-                           (pl.col("PURPOSE.OF.REGISTRATION").str.contains("Z2|Z5")) &
-                           ~(pl.col("BUS.TYPE.STRING").str.contains("CY|12|2F|2R|3I|OH|A7|2U|A8|1D")) 
-                           # & ~(pl.col("SAM.EXTRACT.CODE").str.contains("E|1|4")) #only active records
-                           ).filter((pl.col("NAICS.CODE.STRING").str.contains("Y")) | 
-                           (pl.col("NAICS.EXCEPTION.STRING").str.contains("Y")))
+                           (col("PURPOSE_OF_REGISTRATION").rlike("Z2|Z5")) &
+                           ~(col("BUS_TYPE_STRING").rlike("CY|12|2F|2R|3I|OH|A7|2U|A8|1D")) &
+                            ~(col("SAM_EXTRACT_CODE").rlike("E|1|4")) & #only active records
+                            ((col("NAICS_CODE_STRING").like("%Y%")) | 
+                            (col("NAICS_EXCEPTION_STRING").like("%Y%"))))
     return data
 #%%
 def socioeconomic_filter (data):
@@ -90,12 +58,13 @@ def socioeconomic_filter (data):
 
     options = list(SAM_Extract_dict.keys())
     
-    bus_types = st.sidebar.multiselect("Business/Socioeconomic Types (select multi)", options= options)
+    bus_types = st.sidebar.multiselect("Business/Socioeconomic Types", options= options)
+    st.sidebar.caption ("Combine types for AND filter")
 
     if len(bus_types) > 0:
         lookup_list = [SAM_Extract_dict[x] for x in bus_types]
-        lookupstring = '|'.join(lookup_list)
-        data = data.filter(pl.col("BUS.TYPE.STRING").str.contains(lookupstring))
+        for x in lookup_list:
+            data = data.filter(col("BUS_TYPE_STRING").like(f"%{x}%"))
 
 
     return data
@@ -130,52 +99,47 @@ def state_filter (data):
 
     if len(state_select) > 0:
         lookup_list = [state_names_dict[x] for x in state_select]
-        data = data.filter(pl.col("PHYSICAL.ADDRESS.PROVINCE.OR.STATE").is_in(lookup_list))
+        df_for_in = session.create_dataframe(lookup_list, schema=["col1"])
+        data = data.filter(col("PHYSICAL_ADDRESS_PROVINCE_OR_STATE").isin(df_for_in))
 
     if len(city_select) > 0:
         city_select = city_select.upper()
-        data = data.filter(pl.col("PHYSICAL.ADDRESS.CITY").str.starts_with(city_select))
+        data = data.filter(col("PHYSICAL_ADDRESS_CITY").startswith(city_select))
 
     return data
 
 def display_list_and_count (data):
     col_dict = {
-        "LEGAL.BUSINESS.NAME":"Name",
-        "UNIQUE.ENTITY.ID":"UEI",
-        'SAM.EXTRACT.CODE':"SAM Status (Active/Expired)",
-        "PHYSICAL.ADDRESS.LINE.1":"Address1",
-        "PHYSICAL.ADDRESS.LINE.2":"Address2",
-        "PHYSICAL.ADDRESS.CITY":"City",
-        "PHYSICAL.ADDRESS.PROVINCE.OR.STATE":"State",
-        "PHYSICAL.ADDRESS.ZIP/POSTAL.CODE":"ZIP",
-        "ENTITY.URL":"URL",
-        #,POC:"paste0(GOVT.BUS.POC.FIRST.NAME, " ",GOVT.BUS.POC.LAST.NAME)"
-        "GOVT.BUS.POC.U.S..PHONE":"Phone",
-        "GOVT.BUS.POC.EMAIL.":"Email",
-        "PSC.CODE.STRING":"PSC",
-        'PRIMARY.NAICS':"Primary NAICS",
-        #'NAICS.CODE.STRING':"NAICS String",
-        #'NAICS.EXCEPTION.STRING':"NAICS ",
+        "LEGAL_BUSINESS_NAME":"Name",
+        "UNIQUE_ENTITY_ID":"UEI",
+        'SAM_EXTRACT_CODE':"SAM Status (Active/Expired)",
+        "PHYSICAL_ADDRESS_LINE_1":"Address1",
+        "PHYSICAL_ADDRESS_LINE_2":"Address2",
+        "PHYSICAL_ADDRESS_CITY":"City",
+        "PHYSICAL_ADDRESS_PROVINCE_OR_STATE":"State",
+        "PHYSICAL_ADDRESS_ZIPPOSTAL_CODE":"ZIP",
+        "ENTITY_URL":"URL",
+        "PSC_CODE_STRING":"PSC",
+        'PRIMARY_NAICS':"Primary NAICS",
     }
 
-    addl_cols= ["GOVT.BUS.POC.FIRST.NAME", "GOVT.BUS.POC.LAST.NAME",
-                "NAICS.CODE.STRING", 'NAICS.EXCEPTION.COUNTER',"NAICS.EXCEPTION.STRING"]
+    addl_cols= ["GOVT_BUS_POC_FIRST_NAME", "GOVT_BUS_POC_LAST_NAME",
+                "NAICS_CODE_STRING", 'NAICS_EXCEPTION_COUNTER',"NAICS_EXCEPTION_STRING"]
     
     all_cols = list(col_dict.keys()) + addl_cols
 
-    active_count = data.filter(pl.col("SAM.EXTRACT.CODE")=="A").select(
-        pl.col("UNIQUE.ENTITY.ID").n_unique()).collect().to_pandas()
+    active_count = data.filter(col("SAM_EXTRACT_CODE")=="A").select(countDistinct(col("UNIQUE_ENTITY_ID"))).toPandas().iat[0,0]        
     
-    st.write("Count of Active Registrations: ", str(active_count.iloc[0,0]))
+    st.write("Count of Active Registrations: ", '{:,}'.format(active_count))
 
     if st.button("Show Registrations"):
-        data_df = data.select(all_cols).collect().to_pandas()
-        POC = data_df["GOVT.BUS.POC.FIRST.NAME"] + " " + data_df["GOVT.BUS.POC.LAST.NAME"]
+        data_df = data.select(all_cols).toPandas()
+        POC = data_df["GOVT_BUS_POC_FIRST_NAME"] + " " + data_df["GOVT_BUS_POC_LAST_NAME"]
         data_df.insert(10, "POC", POC)
         
         data_df["NAICS String"] = [x if (y == 0)
                                 else (x + " ~ " + z)
-                                for x, y, z in zip(data_df['NAICS.CODE.STRING'], data_df['NAICS.EXCEPTION.COUNTER'], data_df['NAICS.EXCEPTION.STRING'])]
+                                for x, y, z in zip(data_df['NAICS_CODE_STRING'], data_df['NAICS_EXCEPTION_COUNTER'], data_df['NAICS_EXCEPTION_STRING'])]
         
         data_df = data_df.drop(addl_cols, axis = 1).rename(columns = col_dict)
 
@@ -188,9 +152,8 @@ def display_list_and_count (data):
 
 # %%
 if __name__ == '__main__':
-    #data, date_of = get_data()
-    
-    data, date_of = get_data_local ()
+   
+    data, date_of = get_data ()
     page_config(date_of)
     data = SAM_small_bus (data)
     data = socioeconomic_filter (data)
